9a438fac4106daafbeacbc10ca1893ba6179d5a0
Sathish DK <sathishdk@sathishdk.com>
2015-06-11 17:31:17 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,5 @@
+var app = angular.module("myApp", ['ngAnimate']);
+
+app.controller("MainCtrl", function($scope){
+  $scope.fruits = ["Apple","Apricot","Avocado","Banana","Breadfruit","Bilberry","Blackberry","Blackcurrant","Blueberry","Boysenberry","Cantaloupe","Currant","Cherry","Cherimoya","Cloudberry","Coconut","Cranberry","Cucumber","Damson","Date","Dragonfruit","Durian"];
+});
